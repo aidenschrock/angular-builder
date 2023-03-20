a7c12995b2b8273dff4baeef1e3bfbae937789fb
Aiden Schrock <68505349+aidenschrock@users.noreply.github.com>
2023-03-20 12:46:43 -0500

    middleware test

--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,25 @@
-import { geolocation } from '@vercel/edge';
+// import { geolocation } from '@vercel/edge';
  
-const BLOCKED_COUNTRY = 'US';
+// const BLOCKED_COUNTRY = 'US';
  
-export const config = {
-  // Only run the middleware on the home route
-  matcher: '/',
-};
+// export const config = {
+//   // Only run the middleware on the home route
+//   matcher: '/',
+// };
  
-export default function middleware(request: any) {
-  const url = new URL(request.url);
+// export default function middleware(request: any) {
+//   const url = new URL(request.url);
  
-  const { country } = geolocation(request);
-  // You can also get the country using dot notation on the function
-  // const country = geolocation(request).country;
+//   const { country } = geolocation(request);
+//   // You can also get the country using dot notation on the function
+//   // const country = geolocation(request).country;
  
-  if (country === BLOCKED_COUNTRY) {
-    url.pathname = '/blocked.html';
-  } else {
-    url.pathname = 'index.html';
-  }
+//   if (country === BLOCKED_COUNTRY) {
+//     url.pathname = '/blocked.html';
+//   } else {
+//     url.pathname = 'index.html';
+//   }
  
-  // Return a new redirect response
-  return Response.redirect(url);
-}
+//   // Return a new redirect response
+//   return Response.redirect(url);
+// }
