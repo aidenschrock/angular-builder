c25e50b835e2f27cef7155c3d2a3366832e9bc1c
Aiden Schrock <68505349+aidenschrock@users.noreply.github.com>
2023-03-21 11:47:57 -0500

    middleware cookies

--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,10 @@
-// import { geolocation } from '@vercel/edge';
- 
-// const BLOCKED_COUNTRY = 'US';
- 
-// export const config = {
-//   // Only run the middleware on the home route
-//   matcher: '/',
-// };
- 
-// export default function middleware(request: any) {
-//   const url = new URL(request.url);
- 
-//   const { country } = geolocation(request);
-//   // You can also get the country using dot notation on the function
-//   // const country = geolocation(request).country;
- 
-//   if (country === BLOCKED_COUNTRY) {
-//     url.pathname = '/blocked.html';
-//   } else {
-//     url.pathname = 'index.html';
-//   }
- 
-//   // Return a new redirect response
-//   return Response.redirect(url);
-// }
+import { RequestCookies } from '@edge-runtime/cookies'
+
+export default function middleware(request: any) {
+// if visited cookie doesn't exist, setcookie
+const cookies = new RequestCookies(request.headers)
+if (!cookies.has('visited')) {
+cookies.set('visited', 'true')
+}
+console.log(cookies)
+}
