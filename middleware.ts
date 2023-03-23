d508aac705d9bb0e0c3dccef2eb465c4c3df7edb
Aiden Schrock <68505349+aidenschrock@users.noreply.github.com>
2023-03-23 11:56:06 -0500

    append query params

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,17 @@
 import { RequestCookies } from '@edge-runtime/cookies'
 
+
 export default function middleware(request: any) {
 // if visited cookie doesn't exist, setcookie
 const cookies = new RequestCookies(request.headers)
 if (!cookies.has('visited')) {
 cookies.set('visited', 'true')
+
+} else {
+    // append query parms to URL 
+    let url = new URL(request.url)
+    url.searchParams.append('returningcustomer', 'true');
+    console.log(url)
 }
 console.log(cookies)
 }
